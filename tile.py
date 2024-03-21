02ab2bb105d9731cfd30a2487a8f339bc7975c4d
NicolaCape <nicola.cappellaro.studenti.unitn.it>
2024-03-21 18:01:04 +0100

    Update tile.py

--- tile.py
+++ tile.py
@@ -1,11 +1,17 @@
-# class with 4 attributes: nord, sud, ouest, est
-class Tile:
-    def __init__(self, up, down, left, right):
-        self.up = up
-        self.down = down
-        self.left = left
-        self.right = right
+# classe Tile:
 
-    # check if tow tiles are linkable
+class Tile:
+    def __init__(self, tile_id, cost, num_available, directions):
+        self.tile_id = tile_id
+        self.cost = cost
+        self.num_available = num_available
+        self.directions = directions  # A dictionary with keys 'up', 'down', 'left', 'right'
+
+    def use_tile(self):
+        if self.num_available > 0:
+            self.num_available -= 1
+            return True
+        return False
+
     def linkable(self, other):
-        return self.up == other.down or self.down == other.up or self.left == other.right or self.right == other.left
+        return self.directions["up"] == other.directions["down"] or self.directions["down"] == other.directions["up"] or self.directions["left"] == other.directions["right"] or self.directions["right"] == other["left"]
